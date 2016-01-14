f14a9fe9747ec694e600a9b6131da1542301effc
Luke Klinker <lucas-klinker@uiowa.edu>
2016-01-14 15:56:47 +0000

    update the seeds one more time

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@ event = Event.create!({
   location: "Christ Episcopal Church, Burlington, Iowa",
   active: true,
   title: "Hannah & Lucas",
-  partner_one: "Lucas",
-  partner_two: "Hannah",
+  partner_one: "Hannah",
+  partner_two: "Lucas",
   venue_name: "Christ Episcopal Church",
   venue_address_line_one: "623 N. Fifth St",
   venue_address_city: "Burlington",
@@ -23,7 +23,7 @@ The ceremony will be held at Christ Episcopal Church. The reception, which will 
 
 **Ceremony from I-34 West:** Take the Main St exit and go south a half mile to Court St. Follow Court St for another half mile to arrive at the church.
 
-To get directions [click here](https://www.google.co.uk/maps/dir//Christ+Episcopal+Church,+Burlington,+IA+52601).
+To get directions <a href"https://www.google.co.uk/maps/dir//Christ+Episcopal+Church,+Burlington,+IA+52601" target="_blank" style=target-new: tab;" />
   """
 })
 
